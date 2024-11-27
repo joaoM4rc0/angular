da3d5504992e96a3947daa5120f402cb27fb348c
joaoM4rc0 <joaomarcosantosgit@gmail.com>
2024-11-27 15:54:39 -0300

    estudando angular

---
+++ src/app/_componentes/exemplo-componentes/exemplo-componentes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-componentes',
+  standalone: false,
+  
+  templateUrl: './exemplo-componentes.component.html',
+  styleUrl: './exemplo-componentes.component.css'
+})
+export class ExemploComponentesComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExemploComponentesComponent } from './_componentes/exemplo-componentes/exemplo-componentes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExemploComponentesComponent
   ],
   imports: [
     BrowserModule,
